650c827b7b9ecccf30891586be613e069dc942ad
Sergio Damaso Guerra <alumno@ulpgc.es>
2026-02-24 18:28:50 +0000

    templates format corrected, templates dynamic load implemented

--- index.js
+++ index.js
@@ -1,1 +1,51 @@
-console.log('Happy developing ✨')
+async function xLuIncludeFile() {
+    let z = document.getElementsByTagName("*");
+
+    for (let i = 0; i < z.length; i++) {
+        if (z[i].getAttribute("xlu-include-file")) {
+            let a = z[i].cloneNode(false);
+            let file = z[i].getAttribute("xlu-include-file");
+
+            try {
+                let response = await fetch(file);
+                if (response.ok) {
+
+                    let content = await response.text();
+
+                    // Si el archivo es una plantilla, reemplazamos los placeholders
+                    if (file === "article-template.html") {
+                        let articleData = {
+                            title: z[i].getAttribute("data-title"),
+                            subtitle: z[i].getAttribute("data-subtitle"),
+                            date: z[i].getAttribute("data-date"),
+                            displayDate: z[i].getAttribute("data-display-date"),
+                            content: z[i].getAttribute("data-content"),
+                            image: z[i].getAttribute("data-image"),
+                            imageCaption: z[i].getAttribute("data-image-caption")
+                        };
+
+                        content = content.replace(/{{title}}/g, articleData.title)
+                            .replace(/{{subtitle}}/g, articleData.subtitle)
+                            .replace(/{{date}}/g, articleData.date)
+                            .replace(/{{displayDate}}/g, articleData.displayDate)
+                            .replace(/{{content}}/g, articleData.content)
+                            .replace(/{{image}}/g, articleData.image || '')
+                            .replace(/{{imageCaption}}/g, articleData.imageCaption || '');
+                    }
+
+
+                    a.removeAttribute("xlu-include-file");
+                    //a.innerHTML = await response.text();
+                    a.innerHTML = content;
+                    z[i].parentNode.replaceChild(a, z[i]);
+                    xLuIncludeFile();
+                }
+            } catch (error) {
+                console.error("Error fetching file:", error);
+            }
+
+            return;
+        }
+    }
+}
+
