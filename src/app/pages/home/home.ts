c1b1d08b24760dd68b94be060248d396b050bdc4
Sergio Damaso Guerra <alumno@ulpgc.es>
2026-04-15 00:50:44 +0100

    Implement the teacher components on the home page

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import {TeacherListComponent} from '../../components/teacher-list/teacher-list';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [
-
+    TeacherListComponent
   ],
   templateUrl: './home.html',
   styleUrl: './home.css',
