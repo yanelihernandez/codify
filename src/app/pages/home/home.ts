017954253a859f592a04f4470df0109f7a989ccb
Sergio Damaso Guerra <alumno@ulpgc.es>
2026-04-14 23:01:49 +0100

    Home page component added

---
+++ src/app/pages/home/home.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [
+
+  ],
+  templateUrl: './home.html',
+  styleUrl: './home.css',
+})
+export class HomeComponent {}
+
