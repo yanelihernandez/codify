0f7855e161e91e03dc1208180481eb1016680897
Sergio Damaso Guerra <alumno@ulpgc.es>
2026-04-19 21:25:06 +0100

    Add RouterLink to profile page

--- src/app/pages/profile/profile.ts
+++ src/app/pages/profile/profile.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
 import {TeacherListComponent} from "../../components/teacher-list/teacher-list";
 import {ChatCard} from '../../components/chat-card/chat-card';
+import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-profile',
   standalone: true,
-    imports: [
-        TeacherListComponent,
-        ChatCard
-    ],
+  imports: [
+    TeacherListComponent,
+    ChatCard,
+    RouterLink
+  ],
   templateUrl: './profile.html',
   styleUrl: './profile.css',
 })
