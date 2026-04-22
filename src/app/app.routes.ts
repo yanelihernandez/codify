9e24594a26e963d8b4d73f9d67ee3a9a9f1e0644
Yaneli Perdomo Hernández <127781622+yanelihernandez@users.noreply.github.com>
2026-04-22 13:56:49 +0100

    refactor chat route to accept ID parameter

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,11 @@
 import { Routes } from '@angular/router';
-import {HomeComponent} from './pages/home/home';
-import {LanguageTeachers} from './pages/language-teachers/language-teachers';
-import {TeacherProfileComponent} from './pages/teacher-profile/teacher-profile';
-import {Profile} from './pages/profile/profile';
-import {MyBookings} from './pages/my-bookings/my-bookings';
-import {Chat} from './pages/chat/chat';
-import {Booking} from './pages/booking/booking';
+import { HomeComponent } from './pages/home/home';
+import { LanguageTeachers } from './pages/language-teachers/language-teachers';
+import { TeacherProfileComponent } from './pages/teacher-profile/teacher-profile';
+import { Profile } from './pages/profile/profile';
+import { MyBookings } from './pages/my-bookings/my-bookings';
+import { Chat } from './pages/chat/chat';
+import { Booking } from './pages/booking/booking';
 import { SignInComponent } from './pages/sign-in/sign-in';
 import { SignUpComponent } from './pages/sign-up/sign-up';
 import { EditProfileComponent } from './pages/edit-profile/edit-profile';
@@ -16,7 +16,9 @@ export const routes: Routes = [
   { path: 'teacher-profile/:id', component: TeacherProfileComponent },
   { path: 'profile', component: Profile },
   { path: 'my-bookings', component: MyBookings },
-  { path: 'chat', component: Chat },
+
+  { path: 'chat/:id', component: Chat },
+
   { path: 'booking/:id', component: Booking },
   { path: 'sign-in', component: SignInComponent },
   { path: 'sign-up', component: SignUpComponent },
