42f95866232f3b9490a5895100411fe176200149
Yaneli Perdomo Hernández <127781622+yanelihernandez@users.noreply.github.com>
2026-04-22 13:28:15 +0100

    load user bookings in my bookings page

--- src/app/pages/my-bookings/my-bookings.ts
+++ src/app/pages/my-bookings/my-bookings.ts
@@ -1,13 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
 import { BookingCard } from '../../components/booking-card/booking-card';
-import { RouterLink } from '@angular/router';
+import { BookingItem, BookingService } from '../../services/booking.service';
+import { AuthService } from '../../services/auth';
 
 @Component({
   selector: 'app-my-bookings',
-  imports: [BookingCard, RouterLink],
+  standalone: true,
+  imports: [RouterLink, BookingCard],
   templateUrl: './my-bookings.html',
   styleUrl: './my-bookings.css',
 })
-export class MyBookings {
+export class MyBookings implements OnInit {
+  bookings = signal<BookingItem[]>([]);
 
+  constructor(
+    private bookingService: BookingService,
+    private authService: AuthService,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    const auth = this.authService.authState();
+    console.log('Auth state:', auth);
+
+    if (!auth.loggedIn || !auth.username) {
+      this.router.navigate(['/sign-in']);
+      return;
+    }
+
+    const userBookings = this.bookingService.getBookingsByUser(auth.username);
+    console.log('Reservas cargadas:', userBookings);
+    console.log('Número de reservas:', userBookings.length);
+
+    this.bookings.set(userBookings);
+  }
 }
