aadb7211a622fefb52991e961c222a4213fe3bf0
Yaneli Perdomo Hernández <127781622+yanelihernandez@users.noreply.github.com>
2026-05-02 23:28:17 +0100

    replace import

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import {TeacherListComponent} from '../../components/teacher-list/teacher-list';
+import {TeacherList} from '../../components/teacher-list/teacher-list';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [
-    TeacherListComponent
+    TeacherList
   ],
   templateUrl: './home.html',
   styleUrl: './home.css',
