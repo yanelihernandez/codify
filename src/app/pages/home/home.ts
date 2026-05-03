8122f615b56a8ca07af6b0fd5c5ab9b00f4461ca
Yaneli Perdomo Hernández <127781622+yanelihernandez@users.noreply.github.com>
2026-05-03 19:55:50 +0100

    refactor HomeComponent to use AuthService

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,14 +1,18 @@
-import { Component } from '@angular/core';
-import {TeacherList} from '../../components/teacher-list/teacher-list';
+import { Component, inject } from '@angular/core';
+import { TeacherList } from '../../components/teacher-list/teacher-list';
+import { AuthService } from '../../services/auth';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [
-    TeacherList
-  ],
+  imports: [TeacherList],
   templateUrl: './home.html',
-  styleUrl: './home.css',
+  styleUrl: './home.css'
 })
-export class HomeComponent {}
+export class HomeComponent {
 
+  private authService = inject(AuthService);
+
+  authState = this.authService.authState;
+
+}
