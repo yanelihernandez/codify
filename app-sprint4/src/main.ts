448f16b808a02168a1b059a752793eb0b8743724
Alba Ramos Quintana <albaramosquintana@gmail.com>
2026-05-12 17:22:06 +0100

    reinstalacion de Firebase, configuracion modular en main.ts y estructura core de Ionic

--- app-sprint4/src/main.ts
+++ app-sprint4/src/main.ts
@@ -7,7 +7,7 @@ import { provideHttpClient } from '@angular/common/http';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getAuth, provideAuth } from '@angular/fire/auth';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
-import { environment } from './environments/environment'; 
+import { environment } from './environments/environment';
 
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
